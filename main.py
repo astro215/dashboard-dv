fc49d3333821eb32b33a6448bb5cc2a62c7e8abc
Jainil Patel <jainil.patel.btech2021@sitpune.edu.in>
2023-10-07 23:25:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,15 @@ import streamlit as st
 
 def main():
     st.title("Embedded Power BI Report")
-
+    
     # Embed the Power BI report using an iframe
-    st.components.v1.html(
-        """
-        <div id="embedContainer"></div>
-        <script src="embed_powerbi.js"></script>
-        """,
-        height=600,  # Set the desired height
-        width=800,   # Set the desired width
-        scrolling=True,
-    )
+    iframe_code = """
+    <iframe title="3rd" width="1140" height="541.25" 
+    src="https://app.powerbi.com/reportEmbed?reportId=3c0a1892-aa63-4137-8627-bcad46adbcd2&autoAuth=true&embeddedDemo=true" 
+    frameborder="0" allowFullScreen="true"></iframe>
+    """
+    
+    st.markdown(iframe_code, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
